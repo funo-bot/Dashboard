c5766b4a131d7e69a22af6a7a25cb8cfae75c502
Bradley Watton <mail@bradleyw.me>
2019-04-30 22:04:04 +0100

    Updated build.cmd

--- build.cmd
+++ build.cmd
@@ -1,9 +1,10 @@
 rd /s /q build\dist
+rd /s /q build\.git
 CALL vue-cli-service build --production --dest build/dist
 cd build
 dir
 git init
-git remote set-url origin https://github.com/funo-bot/Dashboard.git
+git remote add origin https://github.com/funo-bot/Dashboard.git
 git add .
 git commit -m "Build"
 git push origin builds --force
