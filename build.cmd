ff737dcc2886d75fbcffd689a080d556e2863afd
Bradley Watton <mail@bradleyw.me>
2019-04-30 21:50:15 +0100

    Uncomment build code

--- build.cmd
+++ build.cmd
@@ -1,8 +1,8 @@
 rmdir build\dist
 vue-cli-service build --production --dest build/dist
-REM cd build
-REM git init
-REM git remote set-url origin https://github.com/funo-bot/Dashboard.git
-REM git add .
-REM git commit -m "Build"
-REM git push origin builds --force
+cd build
+git init
+git remote set-url origin https://github.com/funo-bot/Dashboard.git
+git add .
+git commit -m "Build"
+git push origin builds --force
